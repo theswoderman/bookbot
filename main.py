7240ba6f3ae02a7ca95a70f8f09530f805d3b7ed
theswoderman <nicholas.swider@gmail.com>
2025-05-03 22:30:20 -0700

    cleaned up my code to not require any hard coded paths and use a command argument. also prints a pretty report now!

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from stats import count_words
+from stats import count_characters
+from stats import sort_dictionary
+import sys
+
 def get_book_text(path_to_file):
      with open(path_to_file) as f: 
           file_contents = f.read()
           return file_contents
 
-path_to_file = 'books/frankenstein.txt'
-
 def main():
-     count_words(get_book_text(path_to_file))
-     print(f"{count_words(get_book_text(path_to_file))} words found in the document")
-     
+     if len(sys.argv) < 2:
+          print("Usage: python3 main.py <path_to_book>") 
+          sys.exit(1)
+
+     file_contents = get_book_text(sys.argv[1])
+     word_count = count_words(file_contents)
+     character_count = count_characters(file_contents)
+     list_of_dictionaries = sort_dictionary(character_count)
+     print(f"{word_count} words found in the document")
+     print(f"{character_count}")
+     print(f"{list_of_dictionaries}")
+     print("============ BOOKBOT ============")
+     print("Analyzing book found at books/frankenstein.txt...")
+     print("----------- Word Count ----------")
+     print(f"Found {word_count} total words")
+     print("--------- Character Count -------")
+     for dict in list_of_dictionaries:
+          if dict.get("character").isalpha() == True:
+               print(f"{dict.get("character")}: {dict.get("count")}")
+     print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,27 @@ def count_words(file_contents):
      for word in words:
           word_count += 1
      return word_count
+
+def count_characters(file_contents):
+     character_dictionary = {}
+     characters = list(file_contents)
+     for character in characters:
+          character_lower = character.lower()
+          if((character_lower in character_dictionary) == False):
+               character_dictionary[character_lower] = 1
+          else:
+               character_count = character_dictionary[character_lower]
+               character_count += 1
+               character_dictionary[character_lower] = character_count
+     return character_dictionary
+
+def sort_on(dict):
+     return dict["count"]
+
+def sort_dictionary(character_dictionary):
+     list_of_dictionaries = []
+     for character, count in character_dictionary.items():
+          new_dict = {"character": f"{character}", "count": int(count)}
+          list_of_dictionaries.append(new_dict)
+     list_of_dictionaries.sort(reverse=True, key=sort_on)
+     return list_of_dictionaries
